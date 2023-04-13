ac6d91b1b1350c05403ac8d60258590659175282
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-04-13 17:49:30 +0200

    added call to action component

--- src/components/AppCallToAction.vue
+++ src/components/AppCallToAction.vue
@@ -1,13 +1,64 @@
 <script>
 export default {
     name: "AppContent",
+    data() {
+        return {
+            shopLinks: [
+                {
+                    img: "buy-comics-digital-comics.png",
+                    title: "digital comics",
+                    url: "#",
+                    custom: false
+                },
+                {
+                    img: "buy-comics-merchandise.png",
+                    title: "dc merchandise",
+                    url: "#",
+                    custom: false
+                },
+                {
+                    img: "buy-comics-subscriptions.png",
+                    title: "subscription",
+                    url: "#",
+                    custom: false
+                },
+                {
+                    img: "buy-comics-shop-locator.png",
+                    title: "comic shop locator",
+                    url: "#",
+                    custom: false
+                },
+                {
+                    img: "buy-dc-power-visa.svg",
+                    title: "dc power visa",
+                    url: "#",
+                    custom: false
+                },
+            ]
+        }
+    },
+    methods: {
+        getImgPath(name) {
+
+            return new URL(`../assets/img/${name.img}`, import.meta.url).href;
+        }
+    }
 }
 </script>
 
 <template>
     <section class="call-to-action">
         <div class="container">
-            sono call to action
+            <ul>
+                <li v-for="item in shopLinks">
+                    <a :href="item.url">
+                        <img :class="{ custom: item.custom }" :src="getImgPath(item)" alt="">
+                        <h2>{{ item.title }}</h2>
+                    </a>
+                </li>
+
+            </ul>
+
         </div>
 
     </section>
@@ -21,5 +72,34 @@ export default {
     position: relative;
     background-color: $primary_blue;
     z-index: 10;
+
+    ul {
+        list-style-type: none;
+        display: flex;
+        justify-content: space-around;
+
+        a {
+            color: white;
+            text-decoration: none;
+            display: flex;
+            align-items: center;
+
+            h2 {
+                font-size: .7rem;
+                font-weight: 400;
+                text-transform: uppercase;
+                margin-left: .6rem;
+            }
+        }
+
+        img {
+            width: 40px;
+            height: 40px;
+            object-fit: contain;
+            // &.custom {
+            //     object-fit: contain;
+            // }
+        }
+    }
 }
 </style>
