d33df77aef4b5377054435dc676f8b3c4eb0a87c
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-04-14 13:48:09 +0200

    refactor AppContent added: ContentCard

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,47 +1,86 @@
 <script>
 import data from "../assets/json/dc-comics.json";
+import ContentCard from "./ContentCard.vue";
 /**
  *proprieta delle immagini
- * thumb link
+ * thumb    link
  * price    
  * series   titolo
- * type     casa editrice
+ * type     genere
  *   */
 export default {
     name: "AppContent",
+    components: {
+        ContentCard
+    },
     computed: {
         imgKey() {
             return Object.values(data);
         },
-
     }
 }
 </script>
 
+<!-- <div class="card" v-for="(key, index) in imgKey" :key="index">
+    {{ key.type }}
+</div> -->
+
 <template>
-    <section class="content">
+    <div class="jumbotron">
+        <img src="../assets/img/jumbotron.jpg" alt="">
+    </div>
+    <div class="content">
+
         <div class="container">
-            <div class="card" v-for="(key, index) in imgKey" :key="index">
-                {{ key.type }}
+            <h2 class="label">
+                CURRENT SERIES
+            </h2>
+            <div class="row">
+                <ContentCard v-for="(key, index) in imgKey" :key="index" :title="key.series" :url="key.thumb"
+                    :alt="key.type" />
+
             </div>
-
         </div>
-
-    </section>
+    </div>
 </template>
 
 <style scoped lang="scss">
-.content {
-    background-color: rgb(0, 0, 0);
-    color: white;
-    padding: 2rem 0;
+@use "../style/partials/variables" as *;
 
-    .card {
-        display: inline-block;
-        border: 1px solid white;
-        width: 100px;
-        height: 100px;
-        color: white;
+.jumbotron {
+    height: 180px;
+
+    img {
+        height: 600px;
+        width: 100%;
+        display: block;
+        object-fit: cover;
+    }
+}
+
+.content {
+    position: relative;
+    background-color: $content_bg_color;
+    color: white;
+    z-index: 999;
+
+    .label {
+        background-color: $primary_blue;
+        position: absolute;
+        font-size: .8rem;
+        padding: .5rem 1rem;
+        top: -15px;
+        left: 100px;
+    }
+
+    .container {
+        padding-top: .4rem;
+
+        .row {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+        }
     }
 }
 </style>
